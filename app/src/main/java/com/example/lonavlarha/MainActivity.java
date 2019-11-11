f8c14ec2080a379859e9dd5484f6fd057ee45785
RISHI <rishidhlokheria2001@gmail.com>
2019-11-11 20:31:36 +0530

    first

--- app/src/main/java/com/example/lonavlarha/MainActivity.java
+++ app/src/main/java/com/example/lonavlarha/MainActivity.java
@@ -1,14 +1,44 @@
 package com.example.lonavlarha;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView bigrecycler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        ArrayList<MyModel> upperdata = fill_with_data();
+
+        bigrecycler = findViewById(R.id.up);
+        MyAdapter adapter = new MyAdapter(upperdata);
+        bigrecycler.setAdapter(adapter);
+        bigrecycler.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
     }
-}
+        public ArrayList<MyModel> fill_with_data(){
+
+
+            ArrayList<MyModel> upperdata = new ArrayList<>();
+
+            upperdata.add(new MyModel(R.drawable.image1));
+            upperdata.add(new MyModel(R.drawable.image2));
+            upperdata.add(new MyModel(R.drawable.image3));
+
+
+            return upperdata;
+
+
+        }
+
+
+    }
---
+++ app/src/main/java/com/example/lonavlarha/MyModel.java
@@ -0,0 +1,18 @@
+package com.example.lonavlarha;
+
+public class MyModel {
+
+    int image;
+
+    public MyModel(int image) {
+        this.image = image;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+}
