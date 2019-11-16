9426f7a897e9e6f886c645fba3c2f8f626574ca4
Om <danom1608@gmail.com>
2019-11-16 16:07:19 +0530

    commit 16.11.19(1)

--- app/src/main/java/com/example/lonavlarha/guest_signup.java
+++ app/src/main/java/com/example/lonavlarha/guest_signup.java
@@ -8,21 +8,15 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.auth.PhoneAuthProvider;
 
 public class guest_signup extends AppCompatActivity {
-    EditText email_guest;
-    EditText password_guest;
+    EditText phone_guest;
+    EditText confirm_guest;
     EditText name_guest;
+    TextView confirm;
     Button login_button;
     TextView signin_text;
 
@@ -31,15 +25,18 @@ public class guest_signup extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guest_signup);
 
+        PhoneAuthProvider phoneAuth = PhoneAuthProvider.getInstance();
 
-        email_guest = findViewById(R.id.email_guest);
-        password_guest = findViewById(R.id.password_guest);
+
+        phone_guest = findViewById(R.id.phone_guest);
+        confirm_guest = findViewById(R.id.password_guest);
         login_button = findViewById(R.id.login_button);
+        confirm = findViewById(R.id.password);
         signin_text = findViewById(R.id.signin_text);
         name_guest = findViewById(R.id.name_guest);
 
-        email_guest.setText("");
-        password_guest.setText("");
+        phone_guest.setText("");
+        confirm_guest.setText("");
         name_guest.setText("");
 
 
@@ -48,10 +45,24 @@ public class guest_signup extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                if (email_guest.getText().toString().equals("") || password_guest.getText().toString().equals("") || name_guest.getText().toString().equals("")) {
+                if (phone_guest.getText().toString().equals("") || name_guest.getText().toString().equals("")) {
                     Toast.makeText(guest_signup.this, "Some Information is missing!", Toast.LENGTH_LONG).show();
                 } else {
-                    createAccount();
+                    confirm.setVisibility(View.VISIBLE);
+                    confirm_guest.setVisibility(View.VISIBLE);
+                    login_button.setText("Sign Up");
+
+                    login_button.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            verifyphone(phone_guest.getText().toString());
+                            Toast.makeText(guest_signup.this, "Successfully Signed Up",
+                                    Toast.LENGTH_LONG).show();
+
+                        }
+                    });
+
+                    //createAccount();
                 }
             }
         });
@@ -66,9 +77,14 @@ public class guest_signup extends AppCompatActivity {
 
     }
 
+    private String verifyphone(String phone) {
+
+        return "";
+    }
+
     private void createAccount() {
 
-        final FirebaseAuth guest_auth = FirebaseAuth.getInstance();
+        /*final FirebaseAuth guest_auth = FirebaseAuth.getInstance();
         guest_auth.createUserWithEmailAndPassword(email_guest.getText().toString(), password_guest.getText().toString())
                 .addOnCompleteListener(guest_signup.this, new OnCompleteListener<AuthResult>() {
                     @Override
@@ -79,7 +95,7 @@ public class guest_signup extends AppCompatActivity {
                             DatabaseReference fbdb = FirebaseDatabase.getInstance().getReference().child("Users");
                             Toast.makeText(guest_signup.this, "Successfully Signed Up",
                                     Toast.LENGTH_LONG).show();
-                            fbdb.child(email_guest.getText().toString()).setValue(name_guest.getText().toString());
+                            //fbdb.child(email_guest.getText().toString()).setValue(name_guest.getText().toString());
                             Intent i = new Intent(getApplicationContext(), first_screen_si.class);
                             i.putExtra("User Name",name_guest.getText().toString() );
                             startActivity(i);
@@ -94,6 +110,6 @@ public class guest_signup extends AppCompatActivity {
                     }
 
 
-                });
+                });*/
     }
 }
