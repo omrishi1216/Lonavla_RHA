20bce9d75d559be74cba13ac0a763d89404a8143
RISHI <rishidhlokheria2001@gmail.com>
2019-12-22 15:14:12 +0530

    22.12 team

--- app/src/main/java/com/example/lonavlarha/first_screen_wsi.java
+++ app/src/main/java/com/example/lonavlarha/first_screen_wsi.java
@@ -6,22 +6,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toolbar;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.ArrayList;
 
 public class first_screen_wsi extends AppCompatActivity {
 
-    RecyclerView bigrecycler;
-    Button guest_signup;
+    Button guest_sign;
     Button about_us;
     TextView dev;
-
-    private static final int RC_SIGN_IN = 123;
-
 
 
     @Override
@@ -29,23 +23,15 @@ public class first_screen_wsi extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_screen_wsi);
 
-        guest_signup =  findViewById(R.id.guest);
+
+        guest_sign = findViewById(R.id.guest_wsi);
         about_us = findViewById(R.id.about);
 
-
-
-        ArrayList<MyModel> upperdata = fill_with_data();
-
-        bigrecycler = findViewById(R.id.up);
-        MyAdapter adapter = new MyAdapter(upperdata);
-        bigrecycler.setAdapter(adapter);
-        bigrecycler.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
-
-        guest_signup.setOnClickListener(new View.OnClickListener() {
+        guest_sign.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                startActivity(new Intent(getApplicationContext(),guest_signup.class));
+                startActivity(new Intent(getApplicationContext(), guest_signup.class));
 
             }
         });
@@ -67,21 +53,6 @@ public class first_screen_wsi extends AppCompatActivity {
             }
         });
 
-
     }
 
-    public ArrayList<MyModel> fill_with_data() {
-
-
-        ArrayList<MyModel> upperdata = new ArrayList<>();
-
-        upperdata.add(new MyModel(R.drawable.rha_img));
-        upperdata.add(new MyModel(R.drawable.rha_img2));
-        upperdata.add(new MyModel(R.drawable.rharmy_img));
-        upperdata.add(new MyModel(R.drawable.rha_img4));
-        upperdata.add(new MyModel(R.drawable.image1));
-        upperdata.add(new MyModel(R.drawable.image3));
-
-        return upperdata;
-    }
 }
